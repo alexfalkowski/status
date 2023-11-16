840a50aee92d2167682929dae470f3428c4e21a2
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-16 11:35:03 +0100

    test(deps): update nonnative to v1.74.1

--- test/features/step_definitions/server/v1/transport/http/server_steps.rb
+++ test/features/step_definitions/server/v1/transport/http/server_steps.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
 When('I request to set the code {int} with HTTP') do |code|
-  headers = { request_id: SecureRandom.uuid, user_agent: Status.server_config['transport']['http']['user_agent'] }
+  opts = {
+    headers: {
+      request_id: SecureRandom.uuid, user_agent: Status.server_config['transport']['http']['user_agent'],
+      content_type: :json, accept: :json
+    },
+    read_timeout: 10, open_timeout: 10
+  }
 
-  @response = Status::V1.server_http.code(code, '1ms', headers)
+  @response = Status::V1.server_http.code(code, '1ms', opts)
 end
 
 When('I request to set the invalid code {string} with HTTP') do |code|
-  headers = { request_id: SecureRandom.uuid, user_agent: Status.server_config['transport']['http']['user_agent'] }
+  opts = {
+    headers: {
+      request_id: SecureRandom.uuid, user_agent: Status.server_config['transport']['http']['user_agent'],
+      content_type: :json, accept: :json
+    },
+    read_timeout: 10, open_timeout: 10
+  }
 
-  @response = Status::V1.server_http.code(code, '1ms', headers)
+  @response = Status::V1.server_http.code(code, '1ms', opts)
 end
 
 Then('I should receive a response with {int} from HTTP') do |code|
