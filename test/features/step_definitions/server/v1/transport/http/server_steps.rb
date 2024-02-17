2c1ff7ae1363199414beff2b8fe01a14ce8b6576
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-02-17 08:06:24 +0000

    fix(http): handle error (#89)

--- test/features/step_definitions/server/v1/transport/http/server_steps.rb
+++ test/features/step_definitions/server/v1/transport/http/server_steps.rb
@@ -24,12 +24,24 @@ When('I request to set the invalid code {string} with HTTP') do |code|
   @response = Status::V1.server_http.code(code, '1ms', opts)
 end
 
-Then('I should receive a response with {int} from HTTP') do |code|
-  expect(@response.code).to eq(code)
+When('I request to set the code {string} and invalid {string} for HTTP') do |code, sleep|
+  opts = {
+    headers: {
+      request_id: SecureRandom.uuid, user_agent: Status.server_config.transport.http.user_agent,
+      content_type: :json, accept: :json
+    },
+    read_timeout: 10, open_timeout: 10
+  }
+
+  @response = Status::V1.server_http.code(code, sleep, opts)
+end
+
+Then('I should receive a bad request response from HTTP') do
+  expect(@response.code).to eq(400)
   expect(@response.body.length).to be > 0
 end
 
-Then('I should receive a internal error response from HTTP') do
-  expect(@response.code).to eq(500)
+Then('I should receive a response with {int} from HTTP') do |code|
+  expect(@response.code).to eq(code)
   expect(@response.body.length).to be > 0
 end
