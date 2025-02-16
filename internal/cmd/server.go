c32407758ce8ed5f19efda6ca8858746c0150105
Alex Falkowski <alexrfalkowski@gmail.com>
2025-02-16 20:01:07 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.439.0
    https://github.com/alexfalkowski/go-service/pull/1317

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -16,8 +16,8 @@ import (
 // RegisterServer for cmd.
 func RegisterServer(command *cmd.Command) {
 	flags := flags.NewFlagSet("server")
+	flags.AddInput("env:CONFIG_FILE")
 
-	command.RegisterInput(flags, "env:CONFIG_FILE")
 	command.AddServer("server", "Start status server", flags,
 		module.Module, debug.Module, feature.Module,
 		telemetry.Module, transport.Module,
